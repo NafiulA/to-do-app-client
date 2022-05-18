f9009b8842192c5e77415fa1077ad645cca83079
Nafiul Alam <nafiulalam30@gmail.com>
2022-05-19 00:16:12 +0600

    task delete working

--- src/components/Home/TaskRow.js
+++ src/components/Home/TaskRow.js
@@ -14,13 +14,25 @@ const TaskRow = ({ task, index, refetch }) => {
                 }
             });
     }
+    const handleDelete = (id) => {
+        fetch(`http://localhost:5000/tasks?id=${id}`, {
+            method: "DELETE"
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount) {
+                    toast.success("Task deleted", { id: "taskDelete" });
+                    refetch();
+                }
+            })
+    }
     return (
         <tr>
             <th>{index + 1}</th>
             <td className={`${task.status && "line-through"}`}>{task.task}</td>
             <td className={`${task.status && "line-through"}`}>{task.description}</td>
             <td>{task.status ? "Completed" : <button onClick={() => handleComplete(task._id)} className='btn btn-xs btn-accent'>Complete</button>}</td>
-            <td><button className='btn btn-xs btn-error'>Delete</button></td>
+            <td><button onClick={() => { handleDelete(task._id) }} className='btn btn-xs btn-error'>Delete</button></td>
         </tr>
     );
 };
