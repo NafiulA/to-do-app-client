0e7fae0ccb18b42b40935c35d335f582c08c6491
Nafiul Alam <nafiulalam30@gmail.com>
2022-05-18 23:59:36 +0600

    task complete btn working

--- src/components/Home/TaskRow.js
+++ src/components/Home/TaskRow.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import toast from 'react-hot-toast';
 
 const TaskRow = ({ task, index, refetch }) => {
     const handleComplete = (id) => {
-
+        fetch(`http://localhost:5000/tasks?id=${id}`, {
+            method: "PUT"
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount) {
+                    toast.success("Congrats on finishing the task", { id: "taskDone" });
+                    refetch();
+                }
+            });
     }
-
     return (
         <tr>
             <th>{index + 1}</th>
